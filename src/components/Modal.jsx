cc0a2e5695e001ef8b381704d6a1cf2e08f90e44
yashkemkar <yashkemkar@gmail.com>
2024-12-30 08:29:58 +1100

    Capitalise first letter function
    Some text was showing up uncapitalised for the first letter in the string, so I have written a function to capitalise it.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,3 +1,17 @@
-export default function Modal(){
-    return
+import ReactDom from 'react-dom'
+
+// create modal with portal overlay function that has specifically just the modal content
+export default function Modal(props) {
+    const { children, handleCloseModal } = props
+    return ReactDom.createPortal(
+        // render contents of jsx page without injecting into div id='root'
+        <div className='modal-container'>
+            <button onClick={handleCloseModal} className='modal-underlay' />
+            <div className='modal-content'>
+                {children}
+            </div>
+        </div>,
+        // render modal inside div id='portal' to get outside main application
+        document.getElementById('portal')
+    )
 }
--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { getFullPokedexNumber, getPokedexNumber } from "../utils"
 import TypeCard from "./TypeCard"
+import Modal from "./Modal"
 
 // Need the information to display on the screen once our application is fully loaded.
 
@@ -8,7 +9,8 @@ export default function PokeCard(props) {
     const { selectedPokemon } = props
     const [data, setData] = useState(null)
     const [loading, setLoading] = useState(false) // Need a loading state so we don't send thousands of requests to fetch data
-
+    const [skill, setSkill] = useState(null) //empty state for now
+    const [loadingSkill, setLoadingSkill] = useState(false)
     const { name, height, abilities, stats, types, moves, sprites } = data || {} // the or condition just stipulates if data type is null, destructure out of empty object.
 
     // Need to get a filtered list of images for normal sprites, no other types of images.
@@ -18,8 +20,55 @@ export default function PokeCard(props) {
         return true
     })
 
-    console.log(imgList)
-    // Whenever the selected pokemon changes, execute the function (get request)
+    // Some strings are returning uncapitalised, so created this function to fix that.
+    function capFirstLetter (str) {
+        const newStr = str.charAt(0).toUpperCase() + str.slice(1)
+        return newStr
+    }
+
+    async function fetchMoveData(move, moveUrl) {
+        // guard clauses to prevent fetch request breaking (spam clicking, no data in cache, no url)
+        if (loadingSkill || !localStorage || !moveUrl) { return }
+
+        // check cache for moves object
+        let c = {}
+        if (localStorage.getItem('pokemon-moves')) {
+            c = JSON.parse(localStorage.getItem('pokemon-moves'))
+        }
+
+        // check if move is in moves object (cache)
+        if (move in c) {
+            setSkill(c[move])
+            console.log('Found move in Cache')
+            return
+        }
+
+        // if not, then attempt to fetch data from API
+        try {
+            setLoadingSkill(true)
+            const res = await fetch(moveUrl)
+            const moveData = await res.json()
+            console.log('Fetched move from API', moveData)
+            const description = moveData?.flavor_text_entries.filter(
+                val => {
+                    return val.version_group.name = 'firered-leafgreen'
+                })[0]?.flavor_text
+            const skillData = {
+                name: move,
+                description
+            }
+            setSkill(skillData) // Update skill state to be the move retrieved - this will automatically open the modal as both conditions are met.
+
+            c[move] = skillData // Save the skill in cache
+            localStorage.setItem('pokemon-moves', JSON.stringify(c)) // convert from object back to JSON.
+        } catch (err) {
+            console.log(err.message)
+        } finally {
+            setLoadingSkill(false)
+        }
+    }
+
+    // Whenever the selected pokemon changes, execute the function (get request) using useEffect.
     useEffect(() => {
         // 1. if loading or if localStorage database doesn't exist, exit logic. This is a guard clause to prevent spam
         if (loading || !localStorage) { return }
@@ -41,13 +90,13 @@ export default function PokeCard(props) {
         async function fetchPokemonData() {
             setLoading(true)
             try {
-                const baseurl = 'https://pokeapi.co/api/v2/'
+                const baseUrl = 'https://pokeapi.co/api/v2/'
                 const suffix = 'pokemon/' + getPokedexNumber(selectedPokemon)
-                const finalUrl = baseurl + suffix // dynamic url for fetch api
+                const finalUrl = baseUrl + suffix // dynamic url for fetch api
                 const res = await fetch(finalUrl)
                 const pokemonData = await res.json()
                 setData(pokemonData)
-                console.log(pokemonData)
+                console.log('Fetched data')
                 // save new pokemon information to cache so I can pull from cache next time I run it
                 cache[selectedPokemon] = pokemonData
                 localStorage.setItem('pokemon', JSON.stringify(cache)) // data is an object, so have to stringify it to save into JSON localStorage
@@ -74,12 +123,24 @@ export default function PokeCard(props) {
 
     return (
         <div className='poke-card'>
+            {/* Incorporating Modal for displaying the description of the skills/moves that are shown in the buttons */}
+            {skill && (<Modal handleCloseModal={() => { setSkill(null) }}> {/*require data to render modal.*/}
+                <div>
+                    <h6>Name</h6>
+                    <h2>{capFirstLetter(skill.name.replaceAll('-',' '))}</h2>
+                </div>
+                <div>
+                    <h6>Description</h6>
+                    <p>{capFirstLetter(skill.description)}</p>
+                </div>
+            </Modal>
+            )}
             {/*Name section*/}
             <div>
                 <h4> #{getFullPokedexNumber(selectedPokemon)} </h4>
-                <h2>{name}</h2>
+                <h2>{capFirstLetter(name)}</h2>
             </div>
-            
+
             {/*Type section*/}
             <div className='type-container'>
                 {types.map((typeObj, typeIndex) => {
@@ -96,7 +157,6 @@ export default function PokeCard(props) {
             <div className='img-container'>
                 {imgList.map((spriteUrl, spriteIndex) => {
                     const imgUrl = sprites[spriteUrl]
-                    console.log(imgUrl)
                     return (
                         <img key={spriteIndex} src={imgUrl} alt={`${name}-img-${spriteUrl}`} />
                     )
@@ -105,11 +165,11 @@ export default function PokeCard(props) {
 
             {/*Stats section*/}
             <h3>Stats</h3>
-            <div className = 'stats-card'>
-                {stats.map((statObj,statIndex)=>{
-                    const {stat, base_stat} = statObj
-                    return(
-                        <div className='stat-item'>
+            <div className='stats-card'>
+                {stats.map((statObj, statIndex) => {
+                    const { stat, base_stat } = statObj
+                    return (
+                        <div key={statIndex} className='stat-item'>
                             <p>{stat?.name.replaceAll('-', ' ')}</p> {/* stat name*/}
                             <h4>{base_stat}</h4> {/*stat value*/}
                         </div>
@@ -120,10 +180,12 @@ export default function PokeCard(props) {
             {/*Moves section*/}
             <h3>Moves</h3>
             <div className='pokemon-move-grid'>
-                {moves.map((moveObj,moveIndex)=>{
+                {moves.map((moveObj, moveIndex) => {
                     return (
-                        <button className='button-card pomeon-move' key={moveIndex} oncClick={()=>{}}>
-                            <p>{moveObj?.move?.name.replaceAll('-',' ')}</p>
+                        <button className='button-card pomeon-move' key={moveIndex} onClick={() => {
+                            fetchMoveData(moveObj?.move?.name, moveObj?.move?.url)
+                         }}>
+                            <p>{capFirstLetter(moveObj?.move?.name.replaceAll('-', ' '))}</p>
                         </button>
                     )
                 })}
